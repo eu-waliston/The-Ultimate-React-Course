a6a61985de203d0f6638f90f154318534790bb63
Waliston <goneisgone@hotmail.com>
2023-08-07 08:34:24 -0300

    React Ultmate, Update

---
+++ PT1/exercises/src/Components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Card.scss";
+
+const Card = () => {
+  return (
+    <div className="Card">
+      <h1>CARD EXERCISE</h1>
+    </div>
+  );
+};
+
+export default Card;
