91acc2233cd79b918cca1d4818b3a41acc96ac09
Waliston <goneisgone@hotmail.com>
2023-08-07 13:40:51 -0300

    React Ultmate, Exercise 1

--- PT1/exercises/src/App.jsx
+++ PT1/exercises/src/App.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import "./App.scss";
 import Card from "./Components/Card";
 
-const App = () => {
+const App = (props) => {
   return (
     <div className="App">
-      <Card />
+      <Card 
+        imgURL="./images/download.jpeg"
+        name="Waliston Euripedes"
+        title="Web Developer "
+        about="A web developer is a programmer who develops World Wide Web applications using a client–server model. The applications typically use HTML, CSS, and JavaScript in the client, and any general-purpose programming language in the server. HTTP is used for communications between client and server."
+      />
     </div>
   );
 };
--- PT1/exercises/src/Components/Card.jsx
+++ PT1/exercises/src/Components/Card.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import "./Card.scss";
 
-const Card = () => {
+import { AiFillHtml5 } from "react-icons/ai";
+import {
+  BiLogoCss3,
+  BiLogoJavascript,
+  BiLogoNodejs,
+  BiLogoMongodb,
+} from "react-icons/bi";
+import { BsGit } from "react-icons/bs";
+
+const Card = (props) => {
   return (
     <div className="Card">
-      <h1>CARD EXERCISE</h1>
+      <img src={props.imgURL} alt="Profile" className="profile-picture" />
+      <h1 className="name">{props.name}</h1>
+      <h4 className="title">{props.title}</h4>
+      <p className="about">{props.about}</p>
+      <h4 className="stack">Stacks: </h4>
+      <div className="stacks">
+        <AiFillHtml5 />
+        <BiLogoCss3/>
+        <BiLogoJavascript/>
+        <BiLogoNodejs/>
+        <BiLogoMongodb/>
+      </div>
     </div>
   );
 };
