cb9313148bc077393e0dadb12697352fca95f637
Waliston <goneisgone@hotmail.com>
2023-08-08 20:12:04 -0300

    React Ultmate, Steps Component

--- PT1/state-events-forms/src/App.jsx
+++ PT1/state-events-forms/src/App.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import "./App.css";
 
-const App = () => {
+const messages = [
+  "Learn React 🧠",
+  "Apply for jobs 💼",
+  "Inest your new income 🤑",
+];
+
+export default function App() {
+  const step = 1;
+
   return (
-    <div className="App">
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
 
+      <p className="message">
+        Steep {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
     </div>
-  )
+  );
 }
-
-export default App;
