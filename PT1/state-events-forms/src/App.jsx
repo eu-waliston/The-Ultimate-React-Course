ee7db0a231c679cf193cccb2b436ce5595e0e9ac
Waliston <goneisgone@hotmail.com>
2023-08-09 18:53:42 -0300

    8. Adding Another Piece of State

--- PT1/state-events-forms/src/App.jsx
+++ PT1/state-events-forms/src/App.jsx
@@ -9,6 +9,7 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setisOpen] = useState(true);
 
   function handlePrevius() {
     if (step > 1) {
@@ -23,32 +24,40 @@ export default function App() {
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    <div>
+      <button className="close" onClick={() => setisOpen(!isOpen)}>
+        &times;
+      </button>
 
-      <p className="message">
-        Steep {step}: {messages[step - 1]}
-      </p>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevius}
-        >
-          Previous
-        </button>
+          <p className="message">
+            Steep {step}: {messages[step - 1]}
+          </p>
 
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevius}
+            >
+              Previous
+            </button>
+
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
