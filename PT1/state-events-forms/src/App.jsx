c765ff2e47066c48b62b434071dafcd53eb50feb
Waliston <goneisgone@hotmail.com>
2023-08-08 21:15:17 -0300

    React Ultmate, Steps Component

--- PT1/state-events-forms/src/App.jsx
+++ PT1/state-events-forms/src/App.jsx
@@ -1,20 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 
 const messages = [
   "Learn React 🧠",
   "Apply for jobs 💼",
-  "Inest your new income 🤑",
+  "Invest your new income 🤑",
 ];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+
+  function handlePrevius() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep(step + 1);
+    }
+  }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">
@@ -22,11 +35,17 @@ export default function App() {
       </p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevius}
+        >
           Previous
         </button>
 
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
